add2a606370884e9d15ea3de63ea7ef922b073d8
Dominik Ślusarz <dominik.slusarz@comarch.com>
2023-12-06 16:22:22 +0100

    training part IV, events and external services

--- src/main/java/com/smalaca/orderpreparation/sharedkernel/ChosenProduct.java
+++ src/main/java/com/smalaca/orderpreparation/sharedkernel/ChosenProduct.java
@@ -3,15 +3,23 @@ package com.smalaca.orderpreparation.sharedkernel;
 import com.smalaca.productsmanagement.command.domain.assortment.Price;
 import com.smalaca.productsmanagement.command.domain.assortment.Quantity;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value(staticConstructor = "of")
 public class ChosenProduct {
 
+    @Valid
+    @NotNull
     private final ProductId id;
 
+    @Valid
+    @NotNull
     private final Price price;
 
+    @Valid
+    @NotNull
     private final Quantity quantity;
 
 }
